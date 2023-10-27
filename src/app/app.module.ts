3cc984b79dc02c2fdf3e79848fc77361330c6747
Bhavesh <Bhavesh@mail.com>
2023-10-27 13:48:44 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms'
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,8 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/models/data.service.ts
+++ src/app/models/data.service.ts
@@ -1,127 +1,130 @@
-import { data } from "./data.model";
-import { project } from "./project.model";
-import { skill } from "./skill.model";
+import { Injectable } from '@angular/core';
+import { data } from './data.model';
+import { project } from './project.model';
+import { skill } from './skill.model';
 
-export class DataService{
-    projects: project[] = [
-        {
-          title: 'E-Commerce Platform',
-          desc: 'Full stack application using webapi',
-          link: 'https://gitlab.codetrade.io/trainingtasks/lavneesh-rajput-training-tasks/-/tree/e-commerce-webapi?ref_type=heads',
-        },
-        {
-          title: 'Scholarship Portal',
-          desc: 'MEAN stack application',
-          link: 'https://github.com/lavneesh27/scholarship-portal',
-        },
-        {
-            title: 'Scholarship Portal',
-            desc: 'MEAN stack application',
-            link: 'https://github.com/lavneesh27/scholarship-portal',
-          },
-          {
-            title: 'Scholarship Portal',
-            desc: 'MEAN stack application',
-            link: 'https://github.com/lavneesh27/scholarship-portal',
-          },
-          {
-            title: 'Scholarship Portal',
-            desc: 'MEAN stack application',
-            link: 'https://github.com/lavneesh27/scholarship-portal',
-          },
-          {
-            title: 'Scholarship Portal',
-            desc: 'MEAN stack application',
-            link: 'https://github.com/lavneesh27/scholarship-portal',
-          },
-          {
-            title: 'Scholarship Portal',
-            desc: 'MEAN stack application',
-            link: 'https://github.com/lavneesh27/scholarship-portal',
-          },
-          {
-            title: 'Scholarship Portal',
-            desc: 'MEAN stack application',
-            link: 'https://github.com/lavneesh27/scholarship-portal',
-          },
-        
-      ];
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  projects: project[] = [
+    {
+      title: 'E-Commerce Platform',
+      desc: 'Full stack application using webapi',
+      link: 'https://gitlab.codetrade.io/trainingtasks/lavneesh-rajput-training-tasks/-/tree/e-commerce-webapi?ref_type=heads',
+    },
+    {
+      title: 'Scholarship Portal',
+      desc: 'MEAN stack application',
+      link: 'https://github.com/lavneesh27/scholarship-portal',
+    },
+    {
+      title: 'Scholarship Portal',
+      desc: 'MEAN stack application',
+      link: 'https://github.com/lavneesh27/scholarship-portal',
+    },
+    {
+      title: 'Scholarship Portal',
+      desc: 'MEAN stack application',
+      link: 'https://github.com/lavneesh27/scholarship-portal',
+    },
+    {
+      title: 'Scholarship Portal',
+      desc: 'MEAN stack application',
+      link: 'https://github.com/lavneesh27/scholarship-portal',
+    },
+    {
+      title: 'Scholarship Portal',
+      desc: 'MEAN stack application',
+      link: 'https://github.com/lavneesh27/scholarship-portal',
+    },
+    {
+      title: 'Scholarship Portal',
+      desc: 'MEAN stack application',
+      link: 'https://github.com/lavneesh27/scholarship-portal',
+    },
+    {
+      title: 'Scholarship Portal',
+      desc: 'MEAN stack application',
+      link: 'https://github.com/lavneesh27/scholarship-portal',
+    },
+  ];
 
-      data: data = {
-        "name": "Lavneesh Rajput",
-        "designation": "Full Stack Developer",
-        "work": 9,
-        "project": 10,
-        "certificate": 8,
-        "whatsappNo": "8949254840",
-        "email": "lavirajput376@gmail.com",
-        "linkedIn": "https://www.linkedin.com/in/lavneesh-rajput-59165b1aa/",
-        "github": "https://github.com/lavneesh27",
-        "instagram": "#"
-      };
+  data: data = {
+    name: 'Lavneesh Rajput',
+    designation: 'Full Stack Developer',
+    work: 9,
+    project: 10,
+    certificate: 8,
+    whatsappNo: '8949254840',
+    email: 'lavirajput376@gmail.com',
+    linkedIn: 'https://www.linkedin.com/in/lavneesh-rajput-59165b1aa/',
+    github: 'https://github.com/lavneesh27',
+    instagram: '#',
+  };
 
-      frontEndSkills: skill[] = [
-        {
-          "name": "Angular",
-          "type": "FrontEnd",
-          "proficiency": "Excellent"
-        },
-        {
-          "name": "ReactJS",
-          "type": "FrontEnd",
-          "proficiency": "Intermediate"
-        },
-        {
-          "name": "CSS",
-          "type": "FrontEnd",
-          "proficiency": "Intermediate"
-        },
-        {
-          "name": "Bootstrap",
-          "type": "FrontEnd",
-          "proficiency": "Intermediate"
-        },
-        {
-          "name": "Javascript",
-          "type": "FrontEnd",
-          "proficiency": "Intermediate"
-        },
-        {
-          "name": "Tailwind",
-          "type": "FrontEnd",
-          "proficiency": "Intermediate"
-        }
-      ];
-      backEndSkills: skill[] = [
-        {
-          "name": "NodeJS",
-          "type": "BackEnd",
-          "proficiency": "Intermediate"
-        },
-        {
-          "name": "MongoDB",
-          "type": "BackEnd",
-          "proficiency": "Intermediate"
-        },
-        {
-          "name": "ExpressJS",
-          "type": "BackEnd",
-          "proficiency": "Intermediate"
-        },
-        {
-          "name": "ASP .NET Core",
-          "type": "BackEnd",
-          "proficiency": "Intermediate"
-        },
-        {
-          "name": "MySQL",
-          "type": "BackEnd",
-          "proficiency": "Intermediate"
-        },
-        {
-          "name": "Data Structures",
-          "type": "BackEnd",
-          "proficiency": "Intermediate"
-        }
-      ];
+  frontEndSkills: skill[] = [
+    {
+      name: 'Angular',
+      type: 'FrontEnd',
+      proficiency: 'Excellent',
+    },
+    {
+      name: 'ReactJS',
+      type: 'FrontEnd',
+      proficiency: 'Intermediate',
+    },
+    {
+      name: 'CSS',
+      type: 'FrontEnd',
+      proficiency: 'Intermediate',
+    },
+    {
+      name: 'Bootstrap',
+      type: 'FrontEnd',
+      proficiency: 'Intermediate',
+    },
+    {
+      name: 'Javascript',
+      type: 'FrontEnd',
+      proficiency: 'Intermediate',
+    },
+    {
+      name: 'Tailwind',
+      type: 'FrontEnd',
+      proficiency: 'Intermediate',
+    },
+  ];
+  backEndSkills: skill[] = [
+    {
+      name: 'NodeJS',
+      type: 'BackEnd',
+      proficiency: 'Intermediate',
+    },
+    {
+      name: 'MongoDB',
+      type: 'BackEnd',
+      proficiency: 'Intermediate',
+    },
+    {
+      name: 'ExpressJS',
+      type: 'BackEnd',
+      proficiency: 'Intermediate',
+    },
+    {
+      name: 'ASP .NET Core',
+      type: 'BackEnd',
+      proficiency: 'Intermediate',
+    },
+    {
+      name: 'MySQL',
+      type: 'BackEnd',
+      proficiency: 'Intermediate',
+    },
+    {
+      name: 'Data Structures',
+      type: 'BackEnd',
+      proficiency: 'Intermediate',
+    },
+  ];
 }
